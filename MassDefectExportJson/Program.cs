2d78b506ff68616f723ff1903ce8a8cc53bc4757
thrusanova <thrusanova@gmail.com>
2016-12-10 23:37:43 +0200

    Export Json And Xml

---
+++ MassDefectExportJson/Program.cs
@@ -0,0 +1,70 @@
+﻿using MasssDefect.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassDefectExportJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MassDefectContext();
+            ExportPlanetWhichAreNotAnomalyOrigins(context);
+            ExportPeopleWhichHaveNotBeenVictims(context);
+            ExportTopAnomaly(context);
+        }
+
+        private static void ExportTopAnomaly(MassDefectContext context)
+        {
+            var exportedAnomaly = context.Anomalies
+                 .OrderByDescending(anomaly => anomaly.Victims.Count)
+                 .Take(1)
+                 .Select(anomaly => new
+                 {
+                     id = anomaly.Id,
+                     originPlanet = new
+                     {
+                         name = anomaly.OriginPlanet.Name
+                     },
+                     teleportPlanet = new
+                     {
+                         name = anomaly.TeleportPlanet.Name
+                     },
+                     victimsCount = anomaly.Victims.Count
+                 });
+            var anomalyASJson = JsonConvert.SerializeObject(exportedAnomaly, Formatting.Indented);
+            File.WriteAllText("..//..//..//ExportedResults//anomalies.json", anomalyASJson);
+        }
+
+        private static void ExportPeopleWhichHaveNotBeenVictims(MassDefectContext context)
+        {
+            var people = context.Persons.Where(p => p.Anomalies.Count == 0)
+                .Select(person =>new
+                {
+                    name=person.Name,
+                    homePlanet=new
+                    {
+                        name=person.HomePlanet.Name
+                    }
+                });
+            var peopleASJson = JsonConvert.SerializeObject(people, Formatting.Indented);
+            File.WriteAllText("..//..//..//ExportedResults//people.json", peopleASJson);
+        }
+
+        private static void ExportPlanetWhichAreNotAnomalyOrigins(MassDefectContext contxet)
+        {
+            var exportedPlanets = contxet.PLanets.Where(pl => pl.OriginOfAnomalies.Count==0)
+               .Select(p => new
+               {
+                   name=p.Name
+               });
+            var planetASJson = JsonConvert.SerializeObject(exportedPlanets, Formatting.Indented);
+            File.WriteAllText("..//..//..//ExportedResults//exportedPlanets.json", planetASJson);
+        }
+    }
+}
---
+++ MassDefectExportXML/Program.cs
@@ -0,0 +1,50 @@
+﻿using MasssDefect.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MassDefectExportXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MassDefectContext();
+            var exportedAnomalies = context.Anomalies
+                .Select(anomaly => new
+                {
+                    id = anomaly.Id,
+                    originPlanetName = anomaly.OriginPlanet.Name,
+                    teleportPlanetName = anomaly.TeleportPlanet.Name,
+                    victims = anomaly.Victims.Select(victim => victim.Name)
+                })
+                .OrderBy(exportedAnomaly => exportedAnomaly.id);
+
+            var xmlDocument = new XElement("anomalies");
+
+            foreach (var exportedAnomaly in exportedAnomalies)
+            {
+                var anomalyNode = new XElement("anomaly");
+                anomalyNode.Add(new XAttribute("id", exportedAnomaly.id));
+                anomalyNode.Add(new XAttribute("origin-planet", exportedAnomaly.originPlanetName));
+                anomalyNode.Add(new XAttribute("teleport-planet", exportedAnomaly.teleportPlanetName));
+
+                var victimsNode = new XElement("victims");
+
+                foreach (var victim in exportedAnomaly.victims)
+                {
+                    var victimNode = new XElement("victim");
+                    victimNode.Add(new XAttribute("name", victim));
+                    victimsNode.Add(victimNode);
+                }
+
+                anomalyNode.Add(victimsNode);
+                xmlDocument.Add(anomalyNode);
+            }
+            xmlDocument.Save("..//..//..//ExportedResults//exportedAnomalies.xml");
+        }
+    }
+}
